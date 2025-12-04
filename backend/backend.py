24fdfb1aa7a2d2d260b85a4e6bca7ee43fa0d257
AltairKazuto <ryvbiensent@gmail.com>
2025-12-04 15:03:42 +0800

    v2

--- backend/backend.py
+++ backend/backend.py
@@ -9,11 +9,12 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 model = YOLO('yoga_pose_classifier_v4/weights/best.pt')
+results = model('Goddess-Pose-for-Pose-Page-e1574893769111.jpeg', conf=0.5, save=False, show=True)
 
-@socketio.on("send_image")
-def generate_frames(message):
-	results = model('Goddess-Pose-for-Pose-Page-e1574893769111.jpeg', conf=0.5, save=False, show=False)
-	emit("send_conf", str(results[0].keypoints))
+# @socketio.on("send_image")
+# def generate_frames(message):
+# 	results = model('Goddess-Pose-for-Pose-Page-e1574893769111.jpeg', conf=0.5, save=False, show=False)
+# 	emit("send_conf", str(results[0].keypoints))
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
